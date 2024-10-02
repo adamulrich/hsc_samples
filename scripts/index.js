248a604ab20d5935f0cdb882fd19f8d9e477a2f4
Adam Ulrich <adamulrich@hotmail.com>
2024-10-01 22:12:35 -0700

    disabling buttons on index page that are not wired up.

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,9 @@ function loadCurrentProject() {
     window.location = "project.html?load=__current__"
     
 }
+const currentProjectName = window.localStorage.getItem("currentProjectName")
+if ( currentProjectName!= "") {
+    var btn = document.getElementById("loadProjectBtn");
+    btn.disabled = false;
+    btn.innerText = "Load " + currentProjectName + " Project"
+}
